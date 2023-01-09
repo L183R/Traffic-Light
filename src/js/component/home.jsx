b45f046ec96413c005dd6b5f8d524ba2bcf23077
L183R <liberolascuaga@gmail.com>
2023-01-09 19:53:46 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useState } from 'react';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [color1,setColor1]= useState("btn-secondary");
+	const [color2,setColor2]= useState("btn-secondary");
+	const [color3,setColor3]= useState("btn-secondary");
+	
+	function encenderRojo() {
+        if(color1 === "btn-danger"){
+            setColor1("btn-secondary")
+        }else if(color1 === "btn-secondary"){
+        setColor1("btn-danger");
+		setColor2("btn-secondary");
+		setColor3("btn-secondary");
+    }
+}
+
+    function encenderAmarillo() {
+    if(color2 === "btn-warning"){
+        setColor2("btn-secondary")
+    }else if(color2 === "btn-secondary"){
+        setColor2("btn-warning");
+		setColor1("btn-secondary");
+		setColor3("btn-secondary");
+}
+}
+
+    function encenderVerde() {
+    if(color3 === "btn-success"){
+        setColor3("btn-secondary")
+    }else if(color3 === "btn-secondary"){
+        setColor3("btn-success");
+		setColor1("btn-secondary");
+		setColor2("btn-secondary");
+}
+}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="col-12">
+			<div className="w-25 h-auto p-1 d-flex flex-column" style={{
+      backgroundImage: `url("https://www.nascia.com/wp-content/uploads/2018/07/semaforo-2.jpg")` 
+    }}>
+				<button className={"btn rounded-circle border-danger m-auto mt-3 "+color1} style={{width: 55, height: 55,}} onClick={encenderRojo}></button>
+				<button className={"btn rounded-circle border-warning p-1 m-auto mt-3 "+color2} style={{width: 55, height: 55,}} onClick={encenderAmarillo}></button>
+				<button className={"btn rounded-circle border-warning p-1 m-auto mt-3 "+color3} style={{width: 55, height: 55,}} onClick={encenderVerde}></button>
+			</div>
 		</div>
 	);
 };
